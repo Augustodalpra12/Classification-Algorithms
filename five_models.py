f2fe2f9a9646557379b5807fa00b70e11c081f41
Dal <augusto_dalpra@hotmail.com>
2024-08-07 17:29:02 -0300

    No gridSearch

--- five_models.py
+++ five_models.py
@@ -8,7 +8,7 @@ from sklearn.neighbors import KNeighborsClassifier # knn
 from sklearn.svm import SVC # svc
 from sklearn.tree import DecisionTreeClassifier # ad
 from sklearn.neural_network import MLPClassifier # mlp
-from sklearn.naive_bayes import GaussianNB # nb
+from sklearn.naive_bayes import BernoulliNB # nb
 from sklearn.metrics import accuracy_score
 
 # Abrindo o arquivo para escrita
@@ -26,81 +26,81 @@ with open('results.txt', 'w') as f:
     x_test, x_val, y_test, y_val = train_test_split(x_temp, y_temp, test_size=0.5, stratify=y_temp)
 
     #====================Start of KNN======================================
-    print("KNN", file=f)
-
-    # Definindo os parâmetros que queremos testar no GridSearch
     param_grid_knn = {
         'n_neighbors': list(range(1, 50)),
         'weights': ['uniform', 'distance']
     }
 
-    # Criando o modelo KNN
-    KNN = KNeighborsClassifier()
+    best_accuracy = 0
+    best_params = {}
 
-    # Configurando o GridSearchCV
-    grid_search = GridSearchCV(KNN, param_grid_knn, cv=5, scoring='accuracy', n_jobs=-1, verbose=1)
+    for n_neighbors in param_grid_knn['n_neighbors']:
+        for weight in param_grid_knn['weights']:
+            # Treinando o KNN com os parâmetros atuais
+            knn = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weight)
+            knn.fit(x_train, y_train)
+            
+            # Avaliando no conjunto de validação
+            opiniao = knn.predict(x_val)
+            accuracy_val = accuracy_score(y_val, opiniao)
+            
+            # Se a acurácia atual for melhor que a melhor acurácia, atualize os melhores parâmetros
+            if accuracy_val > best_accuracy:
+                best_accuracy = accuracy_val
+                best_params = {'n_neighbors': n_neighbors, 'weights': weight}
 
-    # Ajustando o GridSearch nos dados de treinamento e validação
-    grid_search.fit(x_val, y_val)
+    # Exibindo os melhores parâmetros
+    print("Melhores Parâmetros: ", best_params)
+    print("Melhor Acurácia no conjunto de validação: ", best_accuracy)
 
-    # Obtendo os melhores parâmetros
-    best_params = grid_search.best_params_
-    print("Melhores Parâmetros: ", best_params, file=f)
-
-    # Treinando o KNN com os melhores parâmetros encontrados
+    # Treinando o modelo final com os melhores parâmetros
     KNN_best = KNeighborsClassifier(n_neighbors=best_params['n_neighbors'], weights=best_params['weights'])
     KNN_best.fit(x_train, y_train)
-
-    # Avaliando no conjunto de validação
-    opiniao = KNN_best.predict(x_val)
-    accuracy_val = accuracy_score(y_val, opiniao)
-    print("Acurácia no conjunto de validação: ", accuracy_val, file=f)
 
     # Avaliando no conjunto de teste
     opiniao_test = KNN_best.predict(x_test)
     accuracy_test = accuracy_score(y_test, opiniao_test)
-    print("Acurácia no conjunto de teste: ", accuracy_test, file=f)
+    print("Acurácia no conjunto de teste: ", accuracy_test)
 
     #====================Start of SVM Classifier================
-    print("\nSVM", file=f)
-
-    # Definindo os parâmetros que queremos testar no GridSearch
     param_grid = {
         'kernel': ['linear', 'rbf', 'poly', 'sigmoid'],
         'C': [0.1, 1, 10, 100, 1000]
     }
 
-    # Criando o modelo SVM
-    svm_model = SVC(random_state=42)
+    best_accuracy = 0
+    best_params = {}
 
-    # Configurando o GridSearchCV
-    grid_search = GridSearchCV(svm_model, param_grid, cv=5, scoring='accuracy', n_jobs=-1, verbose=1)
+    for kernel in param_grid['kernel']:
+        for C in param_grid['C']:
+            # Treinando o SVM com os parâmetros atuais
+            svm_model = SVC(kernel=kernel, C=C, random_state=42)
+            svm_model.fit(x_train, y_train)
+            
+            # Avaliando no conjunto de validação
+            opiniao = svm_model.predict(x_val)
+            accuracy_val = accuracy_score(y_val, opiniao)
+            
+            # Se a acurácia atual for melhor que a melhor acurácia, atualize os melhores parâmetros
+            if accuracy_val > best_accuracy:
+                best_accuracy = accuracy_val
+                best_params = {'kernel': kernel, 'C': C}
 
-    # Ajustando o GridSearch nos dados de validação
-    grid_search.fit(x_val, y_val)
+    # Exibindo os melhores parâmetros
+    print("Melhores Parâmetros: ", best_params)
+    print("Melhor Acurácia no conjunto de validação: ", best_accuracy)
 
-    # Obtendo os melhores parâmetros
-    best_params = grid_search.best_params_
-    print("Melhores Parâmetros: ", best_params, file=f)
-
-    # Treinando o modelo SVM com os melhores parâmetros encontrados
+    # Treinando o modelo final com os melhores parâmetros
     svm_best = SVC(kernel=best_params['kernel'], C=best_params['C'], random_state=42)
     svm_best.fit(x_train, y_train)
-
-    # Avaliando no conjunto de validação
-    opiniao_val = svm_best.predict(x_val)
-    accuracy_val = accuracy_score(y_val, opiniao_val)
-    print("Acurácia no conjunto de validação: ", accuracy_val, file=f)
 
     # Avaliando no conjunto de teste
     opiniao_test = svm_best.predict(x_test)
     accuracy_test = accuracy_score(y_test, opiniao_test)
-    print("Acurácia no conjunto de teste: ", accuracy_test, file=f)
+    print("Acurácia no conjunto de teste: ", accuracy_test)
 
     #====================Start of Decision Tree Classifier================
-    print("\nAD", file=f)
-
-    # Definindo os parâmetros que queremos testar no GridSearch
+    # Definindo os parâmetros que queremos testar
     param_grid = {
         'criterion': ['gini', 'entropy'],
         'max_depth': [None, 10, 20, 30, 40, 50],
@@ -108,20 +108,43 @@ with open('results.txt', 'w') as f:
         'min_samples_leaf': [1, 2, 4, 10]
     }
 
-    # Criando o modelo de Árvore de Decisão
-    decision_tree = DecisionTreeClassifier(random_state=42)
+    best_accuracy = 0
+    best_params = {}
 
-    # Configurando o GridSearchCV
-    grid_search = GridSearchCV(decision_tree, param_grid, cv=5, scoring='accuracy', n_jobs=-1, verbose=1)
+    # Laço para testar todas as combinações de parâmetros
+    for criterion in param_grid['criterion']:
+        for max_depth in param_grid['max_depth']:
+            for min_samples_split in param_grid['min_samples_split']:
+                for min_samples_leaf in param_grid['min_samples_leaf']:
+                    # Treinando o modelo de Árvore de Decisão com os parâmetros atuais
+                    decision_tree = DecisionTreeClassifier(
+                        criterion=criterion,
+                        max_depth=max_depth,
+                        min_samples_split=min_samples_split,
+                        min_samples_leaf=min_samples_leaf,
+                        random_state=42
+                    )
+                    decision_tree.fit(x_train, y_train)
+                    
+                    # Avaliando no conjunto de validação
+                    opiniao_val = decision_tree.predict(x_val)
+                    accuracy_val = accuracy_score(y_val, opiniao_val)
+                    
+                    # Se a acurácia atual for melhor que a melhor acurácia, atualize os melhores parâmetros
+                    if accuracy_val > best_accuracy:
+                        best_accuracy = accuracy_val
+                        best_params = {
+                            'criterion': criterion,
+                            'max_depth': max_depth,
+                            'min_samples_split': min_samples_split,
+                            'min_samples_leaf': min_samples_leaf
+                        }
 
-    # Ajustando o GridSearch nos dados de validação
-    grid_search.fit(x_val, y_val)
+    # Exibindo os melhores parâmetros
+    print("Melhores Parâmetros: ", best_params)
+    print("Melhor Acurácia no conjunto de validação: ", best_accuracy)
 
-    # Obtendo os melhores parâmetros
-    best_params = grid_search.best_params_
-    print("Melhores Parâmetros: ", best_params, file=f)
-
-    # Treinando o modelo de Árvore de Decisão com os melhores parâmetros encontrados
+    # Treinando o modelo final com os melhores parâmetros
     decision_tree_best = DecisionTreeClassifier(
         criterion=best_params['criterion'],
         max_depth=best_params['max_depth'],
@@ -131,41 +154,56 @@ with open('results.txt', 'w') as f:
     )
     decision_tree_best.fit(x_train, y_train)
 
-    # Avaliando no conjunto de validação
-    opiniao_val = decision_tree_best.predict(x_val)
-    accuracy_val = accuracy_score(y_val, opiniao_val)
-    print("Acurácia no conjunto de validação: ", accuracy_val, file=f)
-
     # Avaliando no conjunto de teste
     opiniao_test = decision_tree_best.predict(x_test)
     accuracy_test = accuracy_score(y_test, opiniao_test)
-    print("Acurácia no conjunto de teste: ", accuracy_test, file=f)
+    print("Acurácia no conjunto de teste: ", accuracy_test)
 
     #====================Start of MLP Classifier================
-    print("\nMLP", file=f)
-
-    # Definindo os parâmetros que queremos testar no GridSearch
     param_grid = {
-        'hidden_layer_sizes': [(50,), (100,), (50, 50), (100, 50)],
-        'activation': ['identity', 'logistic', 'tanh', 'relu'],
-        'max_iter': [1000, 1500, 2000],
-        'learning_rate': ['constant', 'invscaling', 'adaptive']
+    'hidden_layer_sizes': [(50,), (100,), (50, 50), (100, 50)],
+    'activation': ['identity', 'logistic', 'tanh', 'relu'],
+    'max_iter': [1000, 1500, 2000],
+    'learning_rate': ['constant', 'invscaling', 'adaptive']
     }
 
-    # Criando o modelo MLP
-    mlp_model = MLPClassifier(random_state=42)
+    best_accuracy = 0
+    best_params = {}
 
-    # Configurando o GridSearchCV
-    grid_search = GridSearchCV(mlp_model, param_grid, cv=5, scoring='accuracy', n_jobs=-1, verbose=1)
+    # Laço para testar todas as combinações de parâmetros
+    for hidden_layer_sizes in param_grid['hidden_layer_sizes']:
+        for activation in param_grid['activation']:
+            for max_iter in param_grid['max_iter']:
+                for learning_rate in param_grid['learning_rate']:
+                    # Treinando o modelo MLP com os parâmetros atuais
+                    mlp_model = MLPClassifier(
+                        hidden_layer_sizes=hidden_layer_sizes,
+                        activation=activation,
+                        max_iter=max_iter,
+                        learning_rate=learning_rate,
+                        random_state=42
+                    )
+                    mlp_model.fit(x_train, y_train)
+                    
+                    # Avaliando no conjunto de validação
+                    opiniao_val = mlp_model.predict(x_val)
+                    accuracy_val = accuracy_score(y_val, opiniao_val)
+                    
+                    # Se a acurácia atual for melhor que a melhor acurácia, atualize os melhores parâmetros
+                    if accuracy_val > best_accuracy:
+                        best_accuracy = accuracy_val
+                        best_params = {
+                            'hidden_layer_sizes': hidden_layer_sizes,
+                            'activation': activation,
+                            'max_iter': max_iter,
+                            'learning_rate': learning_rate
+                        }
 
-    # Ajustando o GridSearch nos dados de validação
-    grid_search.fit(x_val, y_val)
+    # Exibindo os melhores parâmetros
+    print("Melhores Parâmetros: ", best_params)
+    print("Melhor Acurácia no conjunto de validação: ", best_accuracy)
 
-    # Obtendo os melhores parâmetros
-    best_params = grid_search.best_params_
-    print("Melhores Parâmetros: ", best_params, file=f)
-
-    # Treinando o modelo MLP com os melhores parâmetros encontrados
+    # Treinando o modelo final com os melhores parâmetros
     mlp_best = MLPClassifier(
         hidden_layer_sizes=best_params['hidden_layer_sizes'],
         activation=best_params['activation'],
@@ -175,25 +213,18 @@ with open('results.txt', 'w') as f:
     )
     mlp_best.fit(x_train, y_train)
 
-    # Avaliando no conjunto de validação
-    opiniao_val = mlp_best.predict(x_val)
-    accuracy_val = accuracy_score(y_val, opiniao_val)
-    taxa_de_erro_val = 1 - accuracy_val
-    print("Acurácia no conjunto de validação: ", accuracy_val, file=f)
-    print("Taxa de erro no conjunto de validação: ", taxa_de_erro_val, file=f)
-
     # Avaliando no conjunto de teste
     opiniao_test = mlp_best.predict(x_test)
     accuracy_test = accuracy_score(y_test, opiniao_test)
     taxa_de_erro_test = 1 - accuracy_test
-    print("Acurácia no conjunto de teste: ", accuracy_test, file=f)
-    print("Taxa de erro no conjunto de teste: ", taxa_de_erro_test, file=f)
+    print("Acurácia no conjunto de teste: ", accuracy_test)
+    print("Taxa de erro no conjunto de teste: ", taxa_de_erro_test)
 
     #====================Start of Naive Bayes Classifier================
     print("\nNB", file=f)
 
     # Criando e treinando o modelo de Naive Bayes
-    naive_bayes = GaussianNB()
+    naive_bayes = BernoulliNB()
     naive_bayes.fit(x_train, y_train)
 
     # Avaliando no conjunto de validação
