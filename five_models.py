552c5107a97168932b77d0581bea5ae3b609c982
Joao-Biederman <joaobiedetman@gmail.com>
2024-08-13 19:50:03 -0300

    result.csv

--- five_models.py
+++ five_models.py
@@ -10,22 +10,31 @@ from sklearn.tree import DecisionTreeClassifier # ad
 from sklearn.neural_network import MLPClassifier # mlp
 from sklearn.naive_bayes import BernoulliNB # nb
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 # Abrindo o arquivo para escrita
-with open('results.txt', 'w') as f:
+# with open('results.txt', 'w') as txt:
+txt = open('results.txt', 'w')
 
+accuracy_list = []
+for i in range(1, 20):
+    print('Iteração ', i, file=txt)
     #====================data and splitting==============================
     dados = pd.read_csv('database.csv')
     dados = shuffle(dados)
 
     x = dados.iloc[:, [3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]]
+
     # y terá a coluna all_nba
     y = dados.iloc[:, 4]
 
     x_train, x_temp, y_train, y_temp = train_test_split(x, y, test_size=0.5, stratify=y)
     x_test, x_val, y_test, y_val = train_test_split(x_temp, y_temp, test_size=0.5, stratify=y_temp)
 
+    accuracy = []
     #====================Start of KNN======================================
+    print("\nKNN", file=txt)
+
     param_grid_knn = {
         'n_neighbors': list(range(1, 50)),
         'weights': ['uniform', 'distance']
@@ -50,8 +59,8 @@ with open('results.txt', 'w') as f:
                 best_params = {'n_neighbors': n_neighbors, 'weights': weight}
 
     # Exibindo os melhores parâmetros
-    print("Melhores Parâmetros: ", best_params)
-    print("Melhor Acurácia no conjunto de validação: ", best_accuracy)
+    print("Melhores Parâmetros: ", best_params, file=txt)
+    print("Melhor Acurácia no conjunto de validação: ", best_accuracy, file=txt)
 
     # Treinando o modelo final com os melhores parâmetros
     KNN_best = KNeighborsClassifier(n_neighbors=best_params['n_neighbors'], weights=best_params['weights'])
@@ -60,9 +69,17 @@ with open('results.txt', 'w') as f:
     # Avaliando no conjunto de teste
     opiniao_test = KNN_best.predict(x_test)
     accuracy_test = accuracy_score(y_test, opiniao_test)
-    print("Acurácia no conjunto de teste: ", accuracy_test)
+    print("Acurácia no conjunto de teste: ", accuracy_test, file=txt)
+    print("Matriz de Confusão\n ",confusion_matrix(y_test, opiniao_test), file=txt)
+    TN, FP, FN, TP = confusion_matrix(y_test, opiniao_test).ravel()
+    print("Sensibilidade: ",(TP/(TP+FN)), file=txt)
+    print("Especificade: ",(TN/(FP+TN)), file=txt)
+
+    accuracy.append(accuracy_test)
 
     #====================Start of SVM Classifier================
+    print("\nSVM", file=txt)
+
     param_grid = {
         'kernel': ['linear', 'rbf', 'poly', 'sigmoid'],
         'C': [0.1, 1, 10, 100, 1000]
@@ -87,8 +104,8 @@ with open('results.txt', 'w') as f:
                 best_params = {'kernel': kernel, 'C': C}
 
     # Exibindo os melhores parâmetros
-    print("Melhores Parâmetros: ", best_params)
-    print("Melhor Acurácia no conjunto de validação: ", best_accuracy)
+    print("Melhores Parâmetros: ", best_params, file=txt)
+    print("Melhor Acurácia no conjunto de validação: ", best_accuracy, file=txt)
 
     # Treinando o modelo final com os melhores parâmetros
     svm_best = SVC(kernel=best_params['kernel'], C=best_params['C'], random_state=42)
@@ -97,10 +114,17 @@ with open('results.txt', 'w') as f:
     # Avaliando no conjunto de teste
     opiniao_test = svm_best.predict(x_test)
     accuracy_test = accuracy_score(y_test, opiniao_test)
-    print("Acurácia no conjunto de teste: ", accuracy_test)
+    print("Acurácia no conjunto de teste: ", accuracy_test, file=txt)
+    print("Matriz de Confusão\n ",confusion_matrix(y_test, opiniao_test), file=txt)
+    TN, FP, FN, TP = confusion_matrix(y_test, opiniao_test).ravel()
+    print("Sensibilidade: ",(TP/(TP+FN)), file=txt)
+    print("Especificade: ",(TN/(FP+TN)), file=txt)
 
+    accuracy.append(accuracy_test)
     #====================Start of Decision Tree Classifier================
     # Definindo os parâmetros que queremos testar
+    print("\nDecision Tree", file=txt)
+
     param_grid = {
         'criterion': ['gini', 'entropy'],
         'max_depth': [None, 10, 20, 30, 40, 50],
@@ -141,8 +165,8 @@ with open('results.txt', 'w') as f:
                         }
 
     # Exibindo os melhores parâmetros
-    print("Melhores Parâmetros: ", best_params)
-    print("Melhor Acurácia no conjunto de validação: ", best_accuracy)
+    print("Melhores Parâmetros: ", best_params, file=txt)
+    print("Melhor Acurácia no conjunto de validação: ", best_accuracy, file=txt)
 
     # Treinando o modelo final com os melhores parâmetros
     decision_tree_best = DecisionTreeClassifier(
@@ -157,71 +181,79 @@ with open('results.txt', 'w') as f:
     # Avaliando no conjunto de teste
     opiniao_test = decision_tree_best.predict(x_test)
     accuracy_test = accuracy_score(y_test, opiniao_test)
-    print("Acurácia no conjunto de teste: ", accuracy_test)
+    print("Acurácia no conjunto de teste: ", accuracy_test, file=txt)
+    print("Matriz de Confusão\n ",confusion_matrix(y_test, opiniao_test), file=txt)
+    TN, FP, FN, TP = confusion_matrix(y_test, opiniao_test).ravel()
+    print("Sensibilidade: ",(TP/(TP+FN)), file=txt)
+    print("Especificade: ",(TN/(FP+TN)), file=txt)
 
+    accuracy.append(accuracy_test)
     #====================Start of MLP Classifier================
-    param_grid = {
-    'hidden_layer_sizes': [(50,), (100,), (50, 50), (100, 50)],
-    'activation': ['identity', 'logistic', 'tanh', 'relu'],
-    'max_iter': [1000, 1500, 2000],
-    'learning_rate': ['constant', 'invscaling', 'adaptive']
-    }
+    # print("\nMLP", file=txt)
+    # param_grid = {
+    # 'hidden_layer_sizes': [(50,), (100,), (50, 50), (100, 50)],
+    # 'activation': ['identity', 'logistic', 'tanh', 'relu'],
+    # 'max_iter': [1000, 1500, 2000],
+    # 'learning_rate': ['constant', 'invscaling', 'adaptive']
+    # }
 
-    best_accuracy = 0
-    best_params = {}
+    # best_accuracy = 0
+    # best_params = {}
 
-    # Laço para testar todas as combinações de parâmetros
-    for hidden_layer_sizes in param_grid['hidden_layer_sizes']:
-        for activation in param_grid['activation']:
-            for max_iter in param_grid['max_iter']:
-                for learning_rate in param_grid['learning_rate']:
-                    # Treinando o modelo MLP com os parâmetros atuais
-                    mlp_model = MLPClassifier(
-                        hidden_layer_sizes=hidden_layer_sizes,
-                        activation=activation,
-                        max_iter=max_iter,
-                        learning_rate=learning_rate,
-                        random_state=42
-                    )
-                    mlp_model.fit(x_train, y_train)
+    # # Laço para testar todas as combinações de parâmetros
+    # for hidden_layer_sizes in param_grid['hidden_layer_sizes']:
+    #     for activation in param_grid['activation']:
+    #         for max_iter in param_grid['max_iter']:
+    #             for learning_rate in param_grid['learning_rate']:
+    #                 # Treinando o modelo MLP com os parâmetros atuais
+    #                 mlp_model = MLPClassifier(
+    #                     hidden_layer_sizes=hidden_layer_sizes,
+    #                     activation=activation,
+    #                     max_iter=max_iter,
+    #                     learning_rate=learning_rate,
+    #                     random_state=42
+    #                 )
+    #                 mlp_model.fit(x_train, y_train)
                     
-                    # Avaliando no conjunto de validação
-                    opiniao_val = mlp_model.predict(x_val)
-                    accuracy_val = accuracy_score(y_val, opiniao_val)
+    #                 # Avaliando no conjunto de validação
+    #                 opiniao_val = mlp_model.predict(x_val)
+    #                 accuracy_val = accuracy_score(y_val, opiniao_val)
                     
-                    # Se a acurácia atual for melhor que a melhor acurácia, atualize os melhores parâmetros
-                    if accuracy_val > best_accuracy:
-                        best_accuracy = accuracy_val
-                        best_params = {
-                            'hidden_layer_sizes': hidden_layer_sizes,
-                            'activation': activation,
-                            'max_iter': max_iter,
-                            'learning_rate': learning_rate
-                        }
+    #                 # Se a acurácia atual for melhor que a melhor acurácia, atualize os melhores parâmetros
+    #                 if accuracy_val > best_accuracy:
+    #                     best_accuracy = accuracy_val
+    #                     best_params = {
+    #                         'hidden_layer_sizes': hidden_layer_sizes,
+    #                         'activation': activation,
+    #                         'max_iter': max_iter,
+    #                         'learning_rate': learning_rate
+    #                     }
 
-    # Exibindo os melhores parâmetros
-    print("Melhores Parâmetros: ", best_params)
-    print("Melhor Acurácia no conjunto de validação: ", best_accuracy)
+    # # Exibindo os melhores parâmetros
+    # print("Melhores Parâmetros: ", best_params)
+    # print("Melhor Acurácia no conjunto de validação: ", best_accuracy)
 
-    # Treinando o modelo final com os melhores parâmetros
-    mlp_best = MLPClassifier(
-        hidden_layer_sizes=best_params['hidden_layer_sizes'],
-        activation=best_params['activation'],
-        max_iter=best_params['max_iter'],
-        learning_rate=best_params['learning_rate'],
-        random_state=42
-    )
-    mlp_best.fit(x_train, y_train)
+    # # Treinando o modelo final com os melhores parâmetros
+    # mlp_best = MLPClassifier(
+    #     hidden_layer_sizes=best_params['hidden_layer_sizes'],
+    #     activation=best_params['activation'],
+    #     max_iter=best_params['max_iter'],
+    #     learning_rate=best_params['learning_rate'],
+    #     random_state=42
+    # )
+    # mlp_best.fit(x_train, y_train)
 
-    # Avaliando no conjunto de teste
-    opiniao_test = mlp_best.predict(x_test)
-    accuracy_test = accuracy_score(y_test, opiniao_test)
-    taxa_de_erro_test = 1 - accuracy_test
-    print("Acurácia no conjunto de teste: ", accuracy_test)
-    print("Taxa de erro no conjunto de teste: ", taxa_de_erro_test)
+    # # Avaliando no conjunto de teste
+    # opiniao_test = mlp_best.predict(x_test)
+    # accuracy_test = accuracy_score(y_test, opiniao_test)
+    # taxa_de_erro_test = 1 - accuracy_test
+    # print("Acurácia no conjunto de teste: ", accuracy_test)
+    # print("Taxa de erro no conjunto de teste: ", taxa_de_erro_test)
 
+    # accuracy.append(accuracy_test)
     #====================Start of Naive Bayes Classifier================
-    print("\nNB", file=f)
+    print("\nNaive Bayes", file=txt)
+    print("\nNB", file=txt)
 
     # Criando e treinando o modelo de Naive Bayes
     naive_bayes = BernoulliNB()
@@ -231,12 +263,23 @@ with open('results.txt', 'w') as f:
     opiniao_val = naive_bayes.predict(x_val)
     accuracy_val = accuracy_score(y_val, opiniao_val)
     taxa_de_erro_val = 1 - accuracy_val
-    print("Acurácia no conjunto de validação: ", accuracy_val, file=f)
-    print("Taxa de erro no conjunto de validação: ", taxa_de_erro_val, file=f)
+    print("Acurácia no conjunto de validação: ", accuracy_val, file=txt)
+    print("Taxa de erro no conjunto de validação: ", taxa_de_erro_val, file=txt)
 
     # Avaliando no conjunto de teste
     opiniao_test = naive_bayes.predict(x_test)
     accuracy_test = accuracy_score(y_test, opiniao_test)
     taxa_de_erro_test = 1 - accuracy_test
-    print("Acurácia no conjunto de teste: ", accuracy_test, file=f)
-    print("Taxa de erro no conjunto de teste: ", taxa_de_erro_test, file=f)
+    print("Acurácia no conjunto de teste: ", accuracy_test, file=txt)
+    print("Taxa de erro no conjunto de teste: ", taxa_de_erro_test, file=txt)
+    print("Matriz de Confusão\n ",confusion_matrix(y_test, opiniao_test), file=txt)
+    TN, FP, FN, TP = confusion_matrix(y_test, opiniao_test).ravel()
+    print("Sensibilidade: ",(TP/(TP+FN)), file=txt)
+    print("Especificade: ",(TN/(FP+TN)), file=txt)
+    accuracy.append(accuracy_test)
+
+    accuracy_list.append(accuracy)
+    print("\n\n", file=txt)
+
+df = pd.DataFrame(accuracy_list, columns=['KNN', 'SVM', 'Decision Tree', 'Naive Bayes'])
+df.to_csv('results.csv', index=False)
